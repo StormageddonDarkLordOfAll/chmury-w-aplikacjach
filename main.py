1d3269c1645977eec76920757aa0a0eb1f126ff7
veskiii <mikolaj.mlynarczyk@icloud.com>
2023-03-07 18:57:18 +0100

    adding record

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from model import *
 import psycopg2
 from psycopg2.extras import RealDictCursor, DictCursor
 from config import settings
-import json
 
 app = FastAPI()
 DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
@@ -45,13 +44,14 @@ def create_table():
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
 
 def import_data_to_table():
     cursor = conn.cursor()
-    sql = f'''COPY people(country,year,continent,least_developed,life_expectancy,population,co2_emissions,health_expenditure,electric_power_consumption,forest_area,gdp_per_capita,individuals_using_the_internet,military_expenditure,people_practicing_open_defecation,people_using_at_least_basic_drinking_water_services,obesity_among_adults,beer_consumption_per_capita)
+    sql = f'''COPY people(country,year,continent,least_developed,life_expectancy,\
+        population,co2_emissions,health_expenditure,electric_power_consumption,\
+            forest_area,gdp_per_capita,individuals_using_the_internet,military_expenditure,\
+                people_practicing_open_defecation,people_using_at_least_basic_drinking_water_services,\
+                    obesity_among_adults,beer_consumption_per_capita)
     FROM '{settings.CSV_DATASET}'
     DELIMITER ';'
     CSV HEADER;'''
@@ -67,30 +67,27 @@ def import_data_to_table():
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
 
-# GET localhost:8000/items
-@app.post("/items/")
+
+@app.post("/people/")
 async def create_item(item: Item):
+    cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO items (name, description, price, tax) VALUES (%s, %s, %s, %s) RETURNING id",
-        (item.name, item.description, item.price, item.tax)
+        "INSERT INTO people (country,year,continent,least_developed,life_expectancy,\
+        population,co2_emissions,health_expenditure,electric_power_consumption,\
+            forest_area,gdp_per_capita,individuals_using_the_internet,military_expenditure,\
+                people_practicing_open_defecation,people_using_at_least_basic_drinking_water_services,\
+                    obesity_among_adults,beer_consumption_per_capita)\
+                          VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id",
+        (item.country, item.year, item.continent, item.least_developed, item.life_expectancy,\
+         item.population, item.co2_emissions, item.health_expenditure, item.electric_power_consumption,\
+            item.forest_area, item.gdp_per_capita, item.individuals_using_the_internet, item.military_expenditure,\
+                item.people_practicing_open_defecation, item.people_using_at_least_basic_drinking_water_services,\
+                    item.obesity_among_adults, item.beer_consumption_per_capita)
     )
     item_id = cursor.fetchone()[0]
     conn.commit()
     return {"id": item_id}
-
-# GET localhost:8000/items/{item_id}
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    cursor.execute(
-        "SELECT name, description, price, tax FROM items WHERE id = %s",
-        (item_id,)
-    )
-    item = cursor.fetchone()
-    return {"id": item_id, "name": item[0], "description": item[1], "price": item[2], "tax": item[3]}
 
 
 @app.get("/people/")
