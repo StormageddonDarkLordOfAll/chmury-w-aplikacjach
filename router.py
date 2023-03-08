235ba6a1044d19a7bfa242dbcd4586b410cb1c45
veskiii <mikolaj.mlynarczyk@icloud.com>
2023-03-08 10:17:03 +0100

    adding new item to db

--- router.py
+++ router.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter
 from database import get_db
 from dbmodel import Item
+import model
 from typing import Optional
 
 
@@ -15,6 +16,14 @@ async def get_people(db: Session = Depends(get_db), country: Optional[str] = Non
     else:
         people = db.query(Item).all()
     return {'status': 'success', 'results': len(people), 'items': people}
+
+@router.post("/")
+async def create_item(item: model.Item, db: Session = Depends(get_db)):
+    new_item = Item(**item.dict())
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
 
 @router.get("/healthcheck")
 def root():
